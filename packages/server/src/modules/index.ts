19c78dc138dbed4f341df369f0dee19561abe7c3
Duy Nguyen <milonguyen95@outlook.com>
2021-05-03 16:50:45 +0700

    setup typeorm, postgres

--- packages/server/src/modules/index.ts
+++ packages/server/src/modules/index.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ExampleModule } from './example';
+import { configService } from '../config/config.service';
 
 @Module({
-  imports: [ExampleModule, ConfigModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    ExampleModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
--- packages/server/src/utils/env.ts
+++
@@ -1,8 +0,0 @@
-export const env = process.env;
-const {NODE_ENV} = env;
-export const is = {
-  development: NODE_ENV === 'development',
-  prod: NODE_ENV === 'production',
-};
-
-
