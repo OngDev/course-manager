b7dfaaf67141a9252711d0d9fb19d825ae2cb0ad
unknown <tamanhhtml@gmail.com>
2021-05-04 22:56:40 +0700

    fetch mock data

--- packages/client/src/components/Video/VideoList/index.tsx
+++ packages/client/src/components/Video/VideoList/index.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import VideoItem from '../VideoItem'
 import style from './../../../styles/video.css';
 
 const VideoList = () => {
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:8000/api/videos').then(res => res.json()).then(data => {
+            setVideos(data.videos)
+        })
+    }, [])
     return (
         <div className={style.videoList}>
             {
-                new Array(5).fill({
-                    username: 'Dianne Edwards',
-                    subname: '@dianneed',
-                    avatar: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
-                    backgroundVideo: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
-                    time: '81 min',
-                    desc: 'Learning how to create simple Swift applications in 8 lessons'
-                }).map(({ username, subname, avatar, backgroundVideo, time, desc }) => (
+                videos.length > 0 && videos.map(({ username, subname, avatar, backgroundVideo, time, desc }, index) => (
                     <VideoItem 
+                        key={index}
                         username={username} 
                         subname={subname} 
                         avatar={`https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`} 
