6022cd0eea00f80370461c208104b6071e54f4ed
unknown <tamanhhtml@gmail.com>
2021-05-04 22:45:16 +0700

    create mock

---
+++ packages/client/mock/api.ts
@@ -0,0 +1,60 @@
+export default {
+    'GET /api/videos': { videos: [
+        {
+            username: 'Dianne Edwards',
+            subname: '@dianneed',
+            avatar: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            backgroundVideo: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            time: '81 min',
+            desc: 'Learning how to create simple Swift applications in 8 lessons'
+        },
+        {
+            username: 'Dianne Edwards',
+            subname: '@dianneed',
+            avatar: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            backgroundVideo: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            time: '81 min',
+            desc: 'Learning how to create simple Swift applications in 8 lessons'
+        },
+        {
+            username: 'Dianne Edwards',
+            subname: '@dianneed',
+            avatar: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            backgroundVideo: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            time: '81 min',
+            desc: 'Learning how to create simple Swift applications in 8 lessons'
+        },
+        {
+            username: 'Dianne Edwards',
+            subname: '@dianneed',
+            avatar: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            backgroundVideo: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            time: '81 min',
+            desc: 'Learning how to create simple Swift applications in 8 lessons'
+        },
+        {
+            username: 'Dianne Edwards',
+            subname: '@dianneed',
+            avatar: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            backgroundVideo: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            time: '81 min',
+            desc: 'Learning how to create simple Swift applications in 8 lessons'
+        },
+        {
+            username: 'Dianne Edwards',
+            subname: '@dianneed',
+            avatar: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            backgroundVideo: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            time: '81 min',
+            desc: 'Learning how to create simple Swift applications in 8 lessons'
+        },
+        {
+            username: 'Dianne Edwards',
+            subname: '@dianneed',
+            avatar: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            backgroundVideo: `https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`,
+            time: '81 min',
+            desc: 'Learning how to create simple Swift applications in 8 lessons'
+        }
+    ] }
+}
