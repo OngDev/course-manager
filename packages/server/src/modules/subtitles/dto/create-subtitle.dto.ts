42b7b5cf093f1004b74430bd59df59d87237f975
Hiếu Nguyễn <hieu010920@gmail.com>
2021-05-05 00:31:44 +0700

    add subtitle module

--- packages/server/src/modules/subtitles/dto/create-subtitle.dto.ts
+++ packages/server/src/modules/subtitles/dto/create-subtitle.dto.ts
@@ -1,7 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';\
 
 export class CreateSubtitleDto {
+  @ApiProperty({ default: '' })
+  @IsString()
+  @IsNotEmpty()
+  videoId: string;
+
+  @ApiProperty({ default: '' })
+  @IsString()
+  @IsNotEmpty()
+  supporterId: string;
+
   @ApiProperty({ default: 'vn' })
   @IsString()
   @IsNotEmpty()
