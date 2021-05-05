5a22ca6f707ca9a565776b86f4fd964079597977
jorodragon <joro010920@gmail.com>
2021-05-05 22:02:58 +0700

    remove isDate

--- packages/server/src/modules/sub-lines/dto/create-sub-line.dto.ts
+++ packages/server/src/modules/sub-lines/dto/create-sub-line.dto.ts
@@ -1,6 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, IsDate } from 'class-validator';
-import { isDate } from 'util';
 
 export class CreateSubLineDto {
   @ApiProperty({ default: Date.now() })
