7ff302c4224a226596fdc38a9d97a8ef2b602366
hieunv <hieunv.ndm@gmail.com>
2021-05-05 14:09:20 +0700

    edit subtitles module

--- packages/server/src/modules/subtitles/dto/create-subtitle.dto.ts
+++ packages/server/src/modules/subtitles/dto/create-subtitle.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString, IsNumber } from 'class-validator';\
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateSubtitleDto {
   @ApiProperty({ default: '' })
