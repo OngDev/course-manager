80a63aeafd6812bfbe4023c887f4fecea202ac38
jorodragon <joro010920@gmail.com>
2021-05-08 21:52:01 +0700

    add subtitles dto

--- packages/server/src/modules/subtitles/entities/subtitle.entity.ts
+++ packages/server/src/modules/subtitles/entities/subtitle.entity.ts
@@ -1,11 +1,12 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, ManyToOne } from 'typeorm';
 import { IsNotEmpty, IsString } from 'class-validator';
 import { BaseEntity } from 'src/modules/base/base.entity';
+import { Video } from 'src/modules/video/entities/video.entity';
 
 @Entity('subtitle')
 export class Subtitle extends BaseEntity {
-  //   @ManyToOne(() => Video, (video) => video, { cascade: true })
-  //   video: Video;
+  @ManyToOne(() => Video, (video) => video.subtitles, { cascade: true })
+  video: Video;
 
   @Column({ nullable: false })
   @IsString()
