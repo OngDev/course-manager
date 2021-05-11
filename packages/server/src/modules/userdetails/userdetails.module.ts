d816a03ca0e32fd72d35d146603b8fbdfae8e96e
Hiếu Phan <hida@Hieus-MacBook-Air.local>
2021-05-11 17:49:31 +0700

    Init user details service

--- packages/server/src/modules/userdetails/userdetails.module.ts
+++ packages/server/src/modules/userdetails/userdetails.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserdetailsController } from './userdetails.controller';
+import { UserdetailsService } from './userdetails.service';
 
 @Module({
-  controllers: [UserdetailsController]
+  controllers: [UserdetailsController],
+  providers: [UserdetailsService]
 })
 export class UserdetailsModule {
     
