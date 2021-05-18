6636eae6ff0fd0cd95491e9ad4793a7a0d5ba156
Duy Nguyen <milonguyen95@outlook.com>
2021-05-18 14:24:37 +0700

    fix eslint

--- packages/client/components/Video/VideoList/index.tsx
+++ packages/client/components/Video/VideoList/index.tsx
@@ -1,31 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import VideoItem from '../VideoItem'
+import React, { useEffect, useState } from 'react';
 import style from '@styles/video.module.css';
+import VideoItem from '../VideoItem';
 
 const VideoList = () => {
-    const [videos, setVideos] = useState([]);
-    useEffect(() => {
-        fetch('http://localhost:8000/api/videos').then(res => res.json()).then(data => {
-            setVideos(data.videos)
-        })
-    }, [])
-    return (
-        <div className={style.videoList}>
-            {
-                videos.length > 0 && videos.map(({ username, subname, avatar, backgroundVideo, time, desc }, index) => (
-                    <VideoItem 
-                        key={index}
-                        username={username} 
-                        subname={subname} 
-                        avatar={`https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`} 
-                        backgroundVideo={`https://picsum.photos/200/300?random=${Math.floor(Math.random() * 50)}`} 
-                        time={time} 
-                        desc={desc} 
-                    />
-                ))
-            }
-        </div>
-    )
-}
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:8000/api/videos')
+      .then(res => res.json())
+      .then(data => {
+        setVideos(data.videos);
+      });
+  }, []);
+  return (
+    <div className={style.videoList}>
+      {videos.length > 0 &&
+        videos.map(
+          (
+            { username, subname, avatar, backgroundVideo, time, desc },
+            index
+          ) => (
+            <VideoItem
+              key={index}
+              username={username}
+              subname={subname}
+              avatar={`https://picsum.photos/200/300?random=${Math.floor(
+                Math.random() * 50
+              )}`}
+              backgroundVideo={`https://picsum.photos/200/300?random=${Math.floor(
+                Math.random() * 50
+              )}`}
+              time={time}
+              desc={desc}
+            />
+          )
+        )}
+    </div>
+  );
+};
 
-export default VideoList
+export default VideoList;
--- packages/client/components/Video/VideoViewer/index.tsx
+++ packages/client/components/Video/VideoViewer/index.tsx
@@ -1,88 +1,94 @@
 import React, { useCallback, useRef, useState } from 'react';
-import style from '@styles/video.module.css';
-import VideoDuration from '../VideoDuration';
-
+import style from '@styles/Video.module.css';
 import { START, STOP, HD, VOLUME, MUTED_VOLUME, ZOOM } from '@constants/icons';
+import VideoDuration from '../VideoDuration';
 
 const SpeedOptions = ['0.5', '1.0', '1.5'];
 
 interface IProps {
-    urlVideo: string;
+  urlVideo: string;
 }
 
-const VideoViewer: React.FC<IProps> = ({urlVideo}) => {
-    const [isSpeed, setIsSpeed] = useState(false);
-    const [isVideoStarted, setIsVideoStarted] = useState(false);
-    const [isVolumeMuted, setIsVolumeMuted] = useState(false);
-    const [speed, setSpeed] = useState(SpeedOptions[1]);
-    const videoRef = useRef<any>(null);
+const VideoViewer: React.FC<IProps> = ({ urlVideo }) => {
+  const [isSpeed, setIsSpeed] = useState(false);
+  const [isVideoStarted, setIsVideoStarted] = useState(false);
+  const [isVolumeMuted, setIsVolumeMuted] = useState(false);
+  const [speed, setSpeed] = useState(SpeedOptions[1]);
+  const videoRef = useRef<any>(null);
 
-    const videoEnd = useCallback(
-        () => setIsVideoStarted(false),[]
-    )
+  const videoEnd = useCallback(() => setIsVideoStarted(false), []);
 
-    const startVideo = () => {
-        const video = videoRef.current;
-        setIsVideoStarted(!isVideoStarted);
-        if (video) {
-            if(video.paused) video.play(); 
-            else video.pause();
-        }
+  const startVideo = () => {
+    const video = videoRef.current;
+    setIsVideoStarted(!isVideoStarted);
+    if (video) {
+      if (video.paused) video.play();
+      else video.pause();
     }
+  };
 
-    const toggleVolume = () => {
-        const video = videoRef.current;
-        if(video) {
-            video.muted = !video.muted;
-            setIsVolumeMuted(!isVolumeMuted);
-        }
+  const toggleVolume = () => {
+    const video = videoRef.current;
+    if (video) {
+      video.muted = !video.muted;
+      setIsVolumeMuted(!isVolumeMuted);
     }
+  };
 
-    const openFullscreen = () => {
-        const video = videoRef.current;
-        if (video) {
-            if (video.requestFullscreen) {
-                video.requestFullscreen();
-            } else if (video.webkitRequestFullscreen) {
-                video.webkitRequestFullscreen();
-            } else if (video.msRequestFullscreen) {
-                video.msRequestFullscreen();
-            }    
-        }
+  const openFullscreen = () => {
+    const video = videoRef.current;
+    if (video) {
+      if (video.requestFullscreen) {
+        video.requestFullscreen();
+      } else if (video.webkitRequestFullscreen) {
+        video.webkitRequestFullscreen();
+      } else if (video.msRequestFullscreen) {
+        video.msRequestFullscreen();
+      }
     }
+  };
 
-    const setPlaySpeed = (speed: string) => {
-        const video = videoRef.current;
+  const setPlaySpeed = (speed: string) => {
+    const video = videoRef.current;
 
-        if (video) {
-            setSpeed(speed);
-            video.playbackRate = speed;
-        }
+    if (video) {
+      setSpeed(speed);
+      video.playbackRate = speed;
     }
+  };
 
-    return (
-        <div className={style.VideoDetailWrapper}>
-            <video className={style.VideoDetail} src={urlVideo} ref={videoRef}></video>
-            <div className={style.Control}>
-                <span onClick={startVideo}><img src={isVideoStarted ? STOP : START} alt="stop" /></span>
-                <span className="speed" onClick={() => setIsSpeed(!isSpeed)}>
-                    {speed}x
-                    {
-                        isSpeed && (
-                            <ul className={style.SpeedOptions}>
-                                {SpeedOptions.map(speed => <li key={speed} onClick={() => setPlaySpeed(speed)}>{speed}x</li>)}
-                            </ul>        
-                        )
-                    }
-                </span>
-                <VideoDuration videoRef={videoRef} endVideo={videoEnd} />
-                <span onClick={toggleVolume}><img src={isVolumeMuted ? MUTED_VOLUME : VOLUME} alt="volume" /></span>
-                <span><img src={HD} alt="hd" /></span>
-                <span><img src={ZOOM} alt="zoom" onClick={openFullscreen} /></span>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={style.VideoDetailWrapper}>
+      <video className={style.VideoDetail} src={urlVideo} ref={videoRef} />
+      <div className={style.Control}>
+        <span onClick={startVideo}>
+          <img src={isVideoStarted ? STOP : START} alt="stop" />
+        </span>
+        <span className="speed" onClick={() => setIsSpeed(!isSpeed)}>
+          {speed}x
+          {isSpeed && (
+            <ul className={style.SpeedOptions}>
+              {SpeedOptions.map(speed => (
+                <li key={speed} onClick={() => setPlaySpeed(speed)}>
+                  {speed}x
+                </li>
+              ))}
+            </ul>
+          )}
+        </span>
+        <VideoDuration videoRef={videoRef} endVideo={videoEnd} />
+        <span onClick={toggleVolume}>
+          <img src={isVolumeMuted ? MUTED_VOLUME : VOLUME} alt="volume" />
+        </span>
+        <span>
+          <img src={HD} alt="hd" />
+        </span>
+        <span>
+          <img src={ZOOM} alt="zoom" onClick={openFullscreen} />
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default VideoViewer;
-
