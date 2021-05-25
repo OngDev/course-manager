5705f7f8b47c5390827b519f31e57cbfbc551039
Hiếu Phan <hida@Hieus-MacBook-Air.local>
2021-05-25 17:41:12 +0700

    Add relational between  userdetail model and account model

--- packages/server/src/modules/userdetails/model.ts
+++ packages/server/src/modules/userdetails/model.ts
@@ -1,6 +1,10 @@
-import { Entity } from 'typeorm';
+import { Entity, JoinColumn, OneToOne } from 'typeorm';
+import { Account } from '../accounts/model';
 import { BaseEntity } from '../base/base.entity';
 
 @Entity({ name: 'userdetail' })
 export class UserDetail extends BaseEntity {  
+    @OneToOne(type => Account)
+    @JoinColumn()
+    account: Account
 }
